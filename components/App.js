d0890ce782343be434c4ed6decc7eb0350c5f6d1
Martin Hartman <hartmanx1@wp.pl>
2019-09-18 03:30:43 +0200

    fix App.js

--- components/App.js
+++ components/App.js
@@ -16,50 +16,50 @@ App = React.createClass({
             loading: true
         });
         this.getGif(searchingText, function (gif) {
-            this.setState({  /
+            this.setState({
                 loading: false,
                 gif: gif,
-                searchingText: searchingText  
+                searchingText: searchingText
             });
-    }.bind(this));
+        }.bind(this));
     },
 
-getGif: function (searchingText, callback) {
-    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText).data;
-            var gif = {
-                url: data.fixed_width_downsampled_url,
-                sourceUrl: data.url
-            };
-            callback(gif);
-        }
-    };
-    xhr.send();
-},
+    getGif: function (searchingText, callback) {
+        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText).data;
+                var gif = {
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);
+            }
+        };
+        xhr.send();
+    },
 
-render: function () {
+    render: function () {
 
-    var styles = {
-        margin: '0 auto',
-        textAlign: 'center',
-        width: '90%'
-    };
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
 
-    return (
-        <div style={styles}>
-            <h1>Wyszukiwarka GIFow!</h1>
-            <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
-            <Search
-                onSearch={this.handleSearch} />
-            <Gif
-                loading={this.state.loading}
-                url={this.state.gif.url}
-                sourceUrl={this.state.gif.sourceUrl} />
-        </div>
-    );
-}
+        return (
+            <div style={styles}>
+                <h1>Wyszukiwarka GIFow!</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <Search
+                    onSearch={this.handleSearch} />
+                <Gif
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl} />
+            </div>
+        );
+    }
 });
